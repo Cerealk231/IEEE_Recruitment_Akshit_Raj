7db926d3c5d5f6b17fb76ba92f585a7a2794118d
Akshit Raj <akshit30052007.raj@gmail.com>
2025-09-05 23:37:53 +0530

    Added question_3_&_4

--- question_3_&_4.py
+++ question_3_&_4.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#question_3
 # Create 5x5 matrix with random integers between 1 and 100
 matrix = np.random.randint(1, 101, size=(5, 5))
 
@@ -24,3 +25,20 @@ print(normalized_matrix)
 flattened_sorted = np.sort(matrix.flatten())
 print("\nFlattened & Sorted Array:")
 print(flattened_sorted)
+
+#question_4
+middle_matrix = matrix[1:4, 1:4]   # slicing rows 1-3 and cols 1-3
+print("\nMiddle 3x3 Matrix:")
+print(middle_matrix)
+
+# Extract first row of middle matrix
+first_row = middle_matrix[0, :]
+print("\nFirst Row of Middle 3x3:", first_row)
+
+# Extract last column of middle matrix
+last_col = middle_matrix[:, -1]
+print("Last Column of Middle 3x3:", last_col)
+
+# Compute dot product
+dot_product = np.dot(first_row, last_col)
+print("\nDot Product of First Row and Last Column:", dot_product)
